55702753d4e9e6acc3b052c41c7acdefdbde8844
Daniel Iván López García <lopez.garcia1319@gmail.com>
2024-03-15 13:01:46 -0600

    Trabajando con precios
    -Se comenzo a trabajar en PriceService.
    
    -Se creo el request para la creacion de precios.

--- app/Presentation/Services/PriceServices.php
+++ app/Presentation/Services/PriceServices.php
@@ -2,6 +2,15 @@
 
 namespace App\Presentation\Services;
 
+use App\Domain\Repository\PriceRepository;
+use App\Http\Requests\CreatePriceRequest;
+
 class PriceServices{
-    
+    public function __construct(
+        private readonly PriceRepository $priceRepository
+    ){}
+
+    public function create(CreatePriceRequest $request){
+
+    }
 }
